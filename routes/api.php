21b10352de6b31dff1100d5bc222f2407fa99b49
Ivan P <Git1488@gmail.com>
2025-10-21 12:14:39 +0500

    add abort

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Hello World',
+    ]);
+});
+
+
