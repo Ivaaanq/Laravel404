8440fb00b801e26ab96a0440576b2298b056c747
Ivan P <Git1488@gmail.com>
2025-10-21 12:15:29 +0500

    add AbortRoute

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', function() {
+    return 'oawesidfasodpncasoicns/odincswdincf';
+});
+
+Route::delete('posts/{id}', function () {
+    return response()->json([
+        'message' => 'Post deleted successfully'
+    ]);
+});
+
+Route::get('posts/{post}', function () {
+    return 'kjdfnjvdjn';
+});
+
+Route::fallback( function () {
+    return response()->json([
+        'message' => 'Not found',
+        'code' => 404
+
+    ],404);
+});
