5afdda9b5181e2c57e7936ac7bef54ed2b1203a0
Ivan <aegis6467@gmail.com>
2025-10-28 09:32:46 +0500

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('logout', [UserController::class, 'logout']);
+    Route::get('users', [UserController::class, 'index'])->middleware('is_admin');
+});
+
+Route::post('register', [UserController::class, 'register']);
+
+Route::post('login', [UserController::class, 'login']);
+
+Route::get('guest', function () {
+    return 'no This auth';
+});
